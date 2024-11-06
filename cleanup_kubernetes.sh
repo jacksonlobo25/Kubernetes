3b8331da06831ba1f3249885df5e1ba65c2da6ed
Jackson Lobo <jacksonlobo2001@gmail.com>
2024-11-06 11:08:52 +0530

    Dynamic namespace added

--- cleanup_kubernetes.sh
+++ cleanup_kubernetes.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-# This script will delete all Kubernetes resources (pods, services, etc.), configmaps, PVCs, and PVs.
+# This script will delete all Kubernetes resources (pods, services, etc.), configmaps, PVCs, and PVs in the dev and prod namespaces.
 
-echo "Deleting all Kubernetes resources (pods, services, etc.)..."
-kubectl delete all --all
+NAMESPACES=("dev" "prod")
 
-echo "Deleting all ConfigMaps..."
-kubectl delete configmap --all
+for NAMESPACE in "${NAMESPACES[@]}"; do
+  echo "Deleting all Kubernetes resources (pods, services, etc.) in namespace $NAMESPACE..."
+  kubectl delete all --all -n $NAMESPACE
 
-echo "Deleting all PersistentVolumeClaims (PVCs)..."
-kubectl delete pvc --all
+  echo "Deleting all ConfigMaps in namespace $NAMESPACE..."
+  kubectl delete configmap --all -n $NAMESPACE
 
-echo "Deleting all PersistentVolumes (PVs)..."
-kubectl delete pv --all
+  echo "Deleting all PersistentVolumeClaims (PVCs) in namespace $NAMESPACE..."
+  kubectl delete pvc --all -n $NAMESPACE
 
-echo "Deleting all Storage Classes (SCs)..."
-kubectl delete sc --all
+  echo "Deleting all PersistentVolumes (PVs) in namespace $NAMESPACE..."
+  kubectl delete pv --all -n $NAMESPACE
 
-# echo "Deleting Jenkins Service Accounts (SA)..."
-# kubectl delete sa jenkins-admin
+  echo "Deleting all Storage Classes (SCs) in namespace $NAMESPACE..."
+  kubectl delete sc --all -n $NAMESPACE
+done
 
-# echo "Deleting Jenkins ClusterRole (CR)..."
-# kubectl delete clusterrole jenkins-admin
+# echo "Deleting Jenkins Service Accounts (SA) in namespace $NAMESPACE..."
+# kubectl delete sa jenkins-admin -n $NAMESPACE
 
-# echo "Deleting Jenkins ClusterRole Binding(CRB)..."
-# kubectl delete clusterrolebinding jenkins-admin 
+# echo "Deleting Jenkins ClusterRole (CR) in namespace $NAMESPACE..."
+# kubectl delete clusterrole jenkins-admin -n $NAMESPACE
+
+# echo "Deleting Jenkins ClusterRole Binding (CRB) in namespace $NAMESPACE..."
+# kubectl delete clusterrolebinding jenkins-admin -n $NAMESPACE
 
 echo "Cleanup complete."
